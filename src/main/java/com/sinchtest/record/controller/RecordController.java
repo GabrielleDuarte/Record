75e2b80bd6e2fa1a6ba9b8bf788974249086752d
duarte_ <gabrielleduartee3@gmail.com>
2021-04-14 17:44:36 -0300

    Adds a record register route

--- src/main/java/com/sinchtest/record/controller/RecordController.java
+++ src/main/java/com/sinchtest/record/controller/RecordController.java
@@ -1,14 +1,31 @@
 package com.sinchtest.record.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sinchtest.record.entity.Record;
+import com.sinchtest.record.repository.RecordRepository;
+
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping(value = "/apirecord")
 @Api(value = "API REST Record")
 public class RecordController {
 
+	private final RecordRepository recordRepository;
+
+	public RecordController(RecordRepository recordRepository) {
+		super();
+		this.recordRepository = recordRepository;
+	}
 	
+	@PostMapping(value = "/register")
+	@ApiOperation(value = "Register a new record")
+	public void recordSave(@RequestBody Record record) {
+		recordRepository.save(record);
+	}	
 }
